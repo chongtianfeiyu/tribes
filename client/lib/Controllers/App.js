5c0e6cbfd1996735719d93143f9cb2a727b55ed7
System Administrator <root@50761.local>
2012-04-03 16:29:57 +0200

    Added player view class

--- client/lib/Controllers/App.js
+++ client/lib/Controllers/App.js
@@ -11,9 +11,10 @@ Game.Controllers.App = (function(options){
 	var CAMERA_NEAR = 1;
 	var CAMERA_FAR = 10000;
 	//Camera-position (relative to player)
-	var CAMERA_X = 100;
-	var CAMERA_Y = 81;
-	var CAMERA_Z = 100;
+	var CAMERA_X = 0;
+	var CAMERA_Y = -300;
+	var CAMERA_Z = 200;
+	var CAMERA_ROTATION_X = 70 * (Math.PI / 180);
 
 	//Renderer-constants
 	var WIDTH = window.innerWidth;
@@ -32,11 +33,11 @@ Game.Controllers.App = (function(options){
 			_.bindAll(this, "animate", "render", "update", "start");
 			
 			//Initialize camera
-			this.camera = new THREE.Camera( CAMERA_FOV, ASPECT, CAMERA_NEAR, CAMERA_FAR );
-			this.camera.projectionMatrix = THREE.Matrix4.makeOrtho( WIDTH / -2, WIDTH / -2, HEIGHT / -2, HEIGHT / -2, CAMERA_NEAR, CAMERA_FAR );
+			this.camera = new THREE.PerspectiveCamera( CAMERA_FOV, ASPECT, CAMERA_NEAR, CAMERA_FAR );
 			this.camera.position.y = CAMERA_Y;
 			this.camera.position.z = CAMERA_Z;
 			this.camera.position.x = CAMERA_X;
+			this.camera.rotation.x = CAMERA_ROTATION_X;
 
 			//Initialize scene
 			this.scene = new THREE.Scene();
@@ -50,7 +51,17 @@ Game.Controllers.App = (function(options){
 			document.body.appendChild(this.renderer.domElement);
 
 			this.worldView = new Game.Views.World({});
-			this.worldView.init();
+			this.worldView.init(this.scene);
+
+
+			 // add subtle ambient lighting
+	        var ambientLight = new THREE.AmbientLight(0x555555);
+	        this.scene.add(ambientLight);
+	 
+	        // add directional light source
+	        var directionalLight = new THREE.DirectionalLight(0xffffff);
+	        directionalLight.position.set(10, 10, 20);
+	        this.scene.add(directionalLight);
 		},
 
 		start : function() {
@@ -58,8 +69,8 @@ Game.Controllers.App = (function(options){
 		},
 
 		animate : function() {
+			console.log("Animate");
 			requestAnimationFrame( this.animate );
-			
 			this.render();
 		},
 
