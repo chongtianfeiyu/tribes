4dedcd4f5a5ce60301d4b24f12eeb5394400c87e
Julian Duniec <julian.duniec@gmail.com>
2012-04-06 23:18:27 +0200

    Continued work on multiplayer

--- client/lib/Views/World.js
+++ client/lib/Views/World.js
@@ -31,11 +31,13 @@ Game.Views.World = (function(options){
 		},
 
 		setState : function(data) {
+			console.log(data.players.length);
+			//Add players
 			for (var i = data.players.length - 1; i >= 0; i--) {
 				var player = data.players[i];
 				var found = false;
-				for (var i = this.players.length - 1; i >= 0; i--) {
-					var p = this.players[i];
+				for (var j = this.players.length - 1; j >= 0; j--) {
+					var p = this.players[j];
 					if(p.name == player.name)
 					{
 						p.mesh.position.x = player.position.x;
@@ -48,8 +50,8 @@ Game.Views.World = (function(options){
 					var p = new Game.Views.Player();
 					p.name = player.name;
 					p.init();
-					console.log("NOT FOUND, ADDING");
 					this.players.push(p);
+					console.log(this.players.length);
 				}
 			};
 		}
